8b1de08bf40e9944baead107a0da1759090d60a3
Esteban Hernández <mutisantos@hotmail.com>
2017-10-26 22:08:15 -0500

    Inicio de generación procedural

---
+++ Assets/MapGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+	public GameObject[] buildings;
+	public bool finishedLoading;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	private void generateMap(){
+		
+
+
+	}
+
+
+
+}
---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+//*Clase nativa para crear edificios o bloques de calles en el mapa */
+public class Building : MonoBehaviour{
+	//X y Y iniciales
+	public int x;
+	public int y;
+	//Dimensiones del edificio
+	public int height;
+	public int width;
+
+	public Building(int x,int y,int h,int w){
+		this.x = x;
+		this.y = y;
+		this.height = h;
+		this.width = w;
+	}
+
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+	public int getHeight(){
+		return height;
+	}
+	public int getWidth(){
+		return width;
+	}
+
+
+}
